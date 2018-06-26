fe4ede5477602d04f05dd9e3f5c092cb3ab1cc81
Maharoz <mahruj66@gmail.com>
2018-06-26 23:33:23 +0600

    dOUBLE THE ARRAY ELEMENT BY MAPPING

---
+++ DoubledTheNumberOfArray.js
@@ -0,0 +1,17 @@
+var numbers =[1,2,3];
+var doubledNumbers=[];
+
+/*
+**********USING FOR LOOP*******
+ for(i=0;i<numbers.length;i++){
+     doubledNumbers.push(numbers[i]*2);
+ }
+ console.log(doubledNumbers);
+
+ */
+
+
+var doubled =numbers.map(function(number){
+    return number *2;
+});
+console.log(doubled);
