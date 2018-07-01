ad5410ff453ac6be0d6b1f42de8af3261dae4763
Maharoz <mahruj66@gmail.com>
2018-07-01 08:43:19 +0600

    Array destructuring practiced

---
+++ DestructuringArrays.js
@@ -0,0 +1,16 @@
+const companies = [
+    'Google',
+    'Facebook',
+    'Uber'
+];
+
+const [name,name1,name2] = companies;
+
+console.log(name);
+console.log(name1);
+console.log(name2);
+
+/*output 
+Google
+Facebook
+Uber*/
