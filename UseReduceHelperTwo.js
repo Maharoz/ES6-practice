8e2cba919a60ceef3afd7b7307d0ed0c50f0aa02
Maharoz <mahruj66@gmail.com>
2018-11-09 23:41:22 +0600

    update

--- UseReduceHelperTwo.js
+++ UseReduceHelperTwo.js
@@ -5,13 +5,13 @@ var primaryColors =[
 ];
 
 
-
+//previous is here working like an accumulator
 var reduceHelperUse = primaryColors.reduce(function(previous,primaryColor){
 
     previous.push(primaryColor.color);
 
     return previous;
-
+//[] is working like an initial value
 },[]);
 
 console.log(reduceHelperUse);
