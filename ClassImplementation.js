552da0c91ea678995a3ca47f0c7a69857e738fe2
Maharoz <mahruj66@gmail.com>
2018-11-09 23:08:23 +0600

    update

--- ClassImplementation.js
+++ ClassImplementation.js
@@ -1,14 +1,19 @@
 class Car {
+    //constructor
     constructor({title}){
         this.title = title;
     }
 
+    //method
     drive(){
         return 'vroom';
     }
 }
 
+//creating the object and initializing the constructor
 const car = new Car({title:'Toyota'});
 
+//calling the object
 console.log(car);
+//calling the method 
 console.log(car.drive());
